0e5b8d68d19b693c0ab8ca3e4e356b34fb7d9147
TS-devil <62942117+TS-devil@users.noreply.github.com>
2020-12-14 16:55:13 +0530

    Run Frist

---
+++ config.sh
@@ -0,0 +1,13 @@
+apt update && apt upgrade
+apt install python python2 -y
+pip install --upgrade pip
+pip install wheel
+pip install asyncio
+pip install telethon
+pip install colorama
+pip install bs4
+pip install rsa
+pip install requests
+pip install pyaes
+pip install async_generator
+termux-setup-storage
